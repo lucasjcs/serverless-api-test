f7ab687da83ce53ee955c2d725f2b19c8a0250e9
Lucas Jacinto <silval@alunos.utfpr.edu.br>
2018-04-17 19:47:42 -0300

    algumas modificações

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ export class App {
         let dataRes = fetch('https://functions-doc-tcc.azurewebsites.net/api/hello-world-test?name=' + name)
             .then(response => response.json())
             .then(data => this._datar = data)
-            .catch(err => "Ops! Algo deu errado, tente novamente mais tarde!" + err)
         return dataRes
     }
 }
